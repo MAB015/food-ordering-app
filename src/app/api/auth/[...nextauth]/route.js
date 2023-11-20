bf9b0a386df1ad5cceb02b0964def912d4bd3fbe
Miguel Ángel <MAB015@HOTMAIL.COM>
2023-11-20 16:28:32 -0500

    Creating the area to login

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -5,22 +5,14 @@ const handler = NextAuth({
   providers: [
     CredentialsProvider({
       name: "Credentials",
+      credentials: {
         username: { label: "Email", type: "email", placeholder: "text@example.com" },
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
-        // You need to provide your own logic here that takes the credentials
-        // submitted and returns either a object representing a user or value
-        // that is false/null if the credentials are invalid.
-        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-        // You can also use the `req` object to obtain additional parameters
-        // (i.e., the request IP address)
-        const res = await fetch("/your/endpoint", {
-          method: "POST",
-          body: JSON.stringify(credentials),
-          headers: { "Content-Type": "application/json" },
-        });
-        const user = await res.json();
+        console.log(this.credentials)
+
+        const user = await res.json()
 
         // If no error and we have user data, return it
         if (res.ok && user) {
